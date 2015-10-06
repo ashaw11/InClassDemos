39269c16d92e284de395d5fe929724222ba9e2b8
Alex Shaw <ashaw11@studentmail.nait.ca>
2015-10-06 15:30:06 -0600

    Update to Latest and Image insert

--- eRestaurantDemo/eRestaurantSystem/DAL/DTOs/ReservationsByDate.cs
+++ eRestaurantDemo/eRestaurantSystem/DAL/DTOs/ReservationsByDate.cs
@@ -1,15 +1,20 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+#region Additional Namespaces
+using System.Collections;
+#endregion
+
 namespace eRestaurantSystem.DAL.DTOs
 {
-    public class ReservationByDate
+    public class ReservationsByDate
     {
         public string Description { get; set; }
+        //the next variable will hold a collection
+        //of reservation rows.
         public IEnumerable Reservations { get; set; }
     }
 }
